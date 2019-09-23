0968b4cb703b2de3596c8bbd2172c6ae92c78a19
nurbek <foto.nurbek@gmail.com>
2019-09-23 19:56:19 +0600

    Search is easy

---
+++ cities/views.py
@@ -0,0 +1,24 @@
+from django.views.generic import TemplateView, ListView
+from django.db.models import Q
+from .models import City
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class SearchResultsView(ListView):
+    model = City
+    template_name = 'search_results.html'
+
+    # def get_queryset(self):
+    #     return City.objects.filter(
+    #         Q(name__icontains='Bishkek') | Q(state__icontains='Chuy')
+    #     )
+
+    def get_queryset(self):  # new
+        query = self.request.GET.get('q')
+        object_list = City.objects.filter(
+            Q(name__icontains=query) | Q(state__icontains=query)
+        )
+        return object_list
